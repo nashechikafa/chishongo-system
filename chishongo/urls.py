ae422db998c4096a043585b85c4868b15ad4ee0a
Walter Chikafa <chikafawaltermunashe@gmail.com>
2026-03-20 19:58:57 +0200

    Remove temporary live user bootstrap

--- chishongo/urls.py
+++ chishongo/urls.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from django.urls import path
 from core import views
-from core.views import bootstrap_live_users
 
 urlpatterns = [
     path('', views.login_view, name='login'),
@@ -19,5 +18,4 @@ urlpatterns = [
     path('manager-expense-entry/', views.manager_expense_entry, name='manager_expense_entry'),
     path('logout/', views.logout_view, name='logout'),
     path('admin/', admin.site.urls),
-    path('bootstrap-live-users/', bootstrap_live_users, name='bootstrap_live_users'),
 ]
