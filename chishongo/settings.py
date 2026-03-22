828f66b87e3385e40c04035f714c96c46218e364
Walter Chikafa <chikafawaltermunashe@gmail.com>
2026-03-22 15:00:11 +0200

    Connect project to PostgreSQL database

--- chishongo/settings.py
+++ chishongo/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-local-dev-key')
 
-DEBUG = True
+DEBUG = os.environ.get('RENDER') is None
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
